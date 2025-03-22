dc118908c3472401520fc18ffa802f6a08a1293c
Christian Koscielniak Pinto <71388250+ChrisKp1710@users.noreply.github.com>
2025-03-22 19:34:04 +0100

    Aggiorna i percorsi di importazione per i componenti UI e rimuovi l'opzione turbopack dallo script di sviluppo

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -30,7 +30,7 @@ const Navbar: React.FC = () => {
             </div>
             <div className="items-center hidden gap-2 md:flex">
                 <ModeToggle />
-                <Link href="https://github.com/benlhachemi/modifio.git">
+                <Link href="https://github.com/ChrisKp1710/convert-app">
                     <Button
                         variant="default"
                         className="items-center hidden gap-2 bg-orange-600 rounded-full w-fit md:flex"
