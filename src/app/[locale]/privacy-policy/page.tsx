832e4d4a3b96f8327c30ecf01a62868e293e89c5
ChrisKp1710 <chriskp1710@gmail.com>
2025-03-24 13:09:06 +0100

    Rimuovi la dipendenza dal parametro locale nella pagina della Privacy Policy e imposta il formato della data su "en-US"

--- src/app/[locale]/privacy-policy/page.tsx
+++ src/app/[locale]/privacy-policy/page.tsx
@@ -1,31 +1,23 @@
 import { useTranslations } from 'next-intl';
 
-export default function PrivacyPolicy({ params }: { params: { locale: string } }) {
+export default function PrivacyPolicy() {
   const t = useTranslations('privacyPolicy');
 
-  // Usa params.locale per ottenere il locale dal percorso dell'URL
-  const locale = params.locale || 'en';  // Imposta 'en' come fallback se il locale non è definito
+  const sections = t.raw('sections');
 
-  // Ottieni il fuso orario del server
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-  // Imposta la data con il formato dinamico in base alla lingua e al fuso orario dell'utente
-  const formattedDate = new Date().toLocaleDateString(locale, {
+  const formattedDate = new Date().toLocaleDateString("en-US", {
     weekday: "short",
     day: "numeric",
     month: "short",
     year: "numeric",
-    timeZone: timeZone,  // Usa il fuso orario per la formattazione della data
   });
-
-  const sections = t.raw('sections');
 
   return (
     <div className="space-y-8 text-md md:text-lg text-muted-foreground pb-8">
       <p className="text-sm">{t('effectiveDate', { date: formattedDate })}</p>
       <p>{t('intro')}</p>
 
-      {sections.map((section: { title: string; content: string }, index: number) => (
+      {sections.map((section:{ title: string; content: string}, index: number) => (
         <section key={index} className="space-y-2">
           <h2 className="text-xl font-semibold text-black dark:text-white">
             {index + 1}. {section.title}
